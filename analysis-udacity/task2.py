7b68d65406ea5f87f8f368edb9c2704ba9d561bb
Nimisha Manjali <manjan@usi.ch>
2021-03-10 10:45:13 +0100

    added metric information to killed mutants

--- analysis-udacity/task2.py
+++ analysis-udacity/task2.py
@@ -15,6 +15,7 @@ pd.set_option('display.max_colwidth', None)
 STD_SPEED = 'Std(Speed)'
 MEAN_LP = 'Mean(LP)'
 STD_SA = 'Std(SA)'
+metrics = [MEAN_LP, STD_SPEED, STD_SA]
 
 
 # given list of mutants with no crashes or obes ,
@@ -89,11 +90,19 @@ def compute_table_for_metric(org, x, metric):
     return str(killed) if killed is not None else 'killed: false'
 
 
-def filter_killed_mutants(df):
-    mutant_list = [(df.loc[df[MEAN_LP] != 'killed: false']['mutation'].tolist()),
-                   (df.loc[df[STD_SA] != 'killed: false']['mutation'].tolist()),
-                   (df.loc[df[STD_SPEED] != 'killed: false']['mutation'].tolist())]
-    return list(set([item for sublist in mutant_list for item in sublist]))
+def print_killed_mutants(df):
+    for i in metrics:
+        killed_mutants = filter_killed_mutants(df, i)
+        if killed_mutants is not None:
+            pprint(killed_mutants)
+
+
+def filter_killed_mutants(df, metric):
+    lst = df.loc[df[metric] != 'killed: false']['mutation'].tolist()
+    if len(lst) > 0:
+        return 'mutants killed by metric ' + metric + ' : ' + ', '.join(lst)
+    else:
+        return None
 
 
 def compute_and_merge_metric_mutant_tables(df, org_model_data):
@@ -123,8 +132,8 @@ if __name__ == "__main__":
     table_mutants_killed_for_no_crashes_obes = compute_and_merge_metric_mutant_tables(df_no_crashes_obes,
                                                                                       org_model_data)
     print(
-        'mutants killed by metrics : Mean(LP), Std(Speed), Std(SA) from mutant list not having crashes or obes_________')
-    pprint(filter_killed_mutants(table_mutants_killed_for_no_crashes_obes))
+        'mutants killed by metrics from mutant list not having crashes or obes_________')
+    (print_killed_mutants(table_mutants_killed_for_no_crashes_obes))
 
     some_crashes_obes_list = build_mutant_list_having_crashes_obes_on_some_models(df_crashes_obes)
     df_some_crashes_obes = extract_data_based_given_mutant_list(sys.argv[1],
@@ -132,5 +141,5 @@ if __name__ == "__main__":
     table_mutants_killed_for_some_crashes_obes = compute_and_merge_metric_mutant_tables(df_some_crashes_obes,
                                                                                         org_model_data)
     print(
-        'mutants killed by metrics : Mean(LP), Std(Speed), Std(SA) from mutant list having some crashes or obes_________')
-    pprint(filter_killed_mutants(table_mutants_killed_for_some_crashes_obes))
+        'mutants killed by metrics from mutant list having some crashes or obes_________')
+    (print_killed_mutants(table_mutants_killed_for_some_crashes_obes))
